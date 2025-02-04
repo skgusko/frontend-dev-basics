72199bd3ac446523a04b64c1b42c8ceaef168436
Konahyeon <koo@inha.edu>
2025-02-04 16:44:23 +0900

    js #3

--- javascript-practices/basics/ex09.01.js
+++ javascript-practices/basics/ex09.01.js
@@ -24,13 +24,17 @@ console.log(str6);
 // 객체 함수
 var str7 = "string1 string2 string3";
 
-var index = str7.indexOf("string2");
-console.log(index); //8
+var index = str7.indexOf("string2"); // 8
+console.log(index);
 
-var index = str7.indexOf("string3");
-console.log(index); //8
+var index = str7.indexOf("string4");  // -1
+console.log(index);
 
-var str8 = str7.substring(10 /*firstIndex*/, 13 /*lastIndex - 1*/);
+var str8 = str7.substring(10 /*firstIndex */, 13 /*lastIndx - 1*/);
 console.log(str8);
 
 var a = str7.split(" ");
+console.log(a);
+
+a = str7.split(":");
+console.log(a);
